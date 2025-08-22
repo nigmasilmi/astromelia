1fbd9eb7cc7b4db094632bcdf44a71e278c9fb2d
Nigma Silmi <sn.silmi@gmail.com>
2025-08-22 12:26:35 -0400

    attaching shadow dom

--- src/web-components/Tooltip/Tooltip.js
+++ src/web-components/Tooltip/Tooltip.js
@@ -6,6 +6,9 @@ class Tooltip extends HTMLElement {
     this._tooltipContainer;
     this._tooltipId = `tip-${Math.random().toString(36).slice(2)}`;
     this._tooltipText = "placeholder for default";
+    // attaching a shadow dom tree to this wc
+    // enables to access this shadow dom tree from outside this wc
+    this.attachShadow({ mode: "open" });
   }
   // connectedCallback runs when the element is added to the DOM.
   // Use it to set up or render the component’s initial content.
@@ -21,7 +24,7 @@ class Tooltip extends HTMLElement {
     // and not only the span which triggers the event
     tooltipIcon.addEventListener("mouseenter", this._showTooltip.bind(this));
     tooltipIcon.addEventListener("mouseleave", this._hideTooltip.bind(this));
-    this.appendChild(tooltipIcon);
+    this.shadowRoot.appendChild(tooltipIcon);
   }
 
   // underscore as a convention to signal this is a "private" method
@@ -31,11 +34,11 @@ class Tooltip extends HTMLElement {
     this._tooltipContainer.id = this.tooltipId;
     this._tooltipContainer.setAttribute("role", "tooltip");
     this._tooltipContainer.textContent = this._tooltipText;
-    this.appendChild(this._tooltipContainer);
+    this.shadowRoot.appendChild(this._tooltipContainer);
   }
 
   _hideTooltip() {
-    this.removeChild(this._tooltipContainer);
+    this.shadowRoot.removeChild(this._tooltipContainer);
   }
 }
 
